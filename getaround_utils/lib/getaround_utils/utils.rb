5a7b3c04485bd4f46c4d62e089c01c335575661f
godric <lahumez@gmail.com>
2020-02-15 12:11:02 +0100

    getaround_utils: Add Utils::CapturReport supporting batch events

--- getaround_utils/lib/getaround_utils/utils.rb
+++ getaround_utils/lib/getaround_utils/utils.rb
@@ -2,4 +2,5 @@ module GetaroundUtils::Utils
   autoload :AsyncQueue, 'getaround_utils/utils/async_queue'
   autoload :DeepKeyValue, 'getaround_utils/utils/deep_key_value'
   autoload :HttpReporter, 'getaround_utils/utils/http_reporter'
+  autoload :CapturReporter, 'getaround_utils/utils/captur_reporter'
 end
