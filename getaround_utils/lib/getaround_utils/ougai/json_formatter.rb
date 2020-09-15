2ecf84696e985716a4e1a85c359dd76aa4e1996d
godric <lahumez@gmail.com>
2020-09-15 11:22:34 +0200

    getaround_utils: Use msg instead of message in Ougai::JsonFormatter

--- getaround_utils/lib/getaround_utils/ougai/json_formatter.rb
+++ getaround_utils/lib/getaround_utils/ougai/json_formatter.rb
@@ -7,8 +7,7 @@ module GetaroundUtils::Ougai; end
 
 class GetaroundUtils::Ougai::JsonFormatter < Ougai::Formatters::Base
   def _call(severity, _time, progname, data)
-    message = data.delete(:msg)
-    data[:message] = message if message != 'No message'
+    data.delete(:msg) if message == 'No message'
 
     payload = { severity: severity, progname: progname }.merge(data).compact!
     JSON.dump(payload) + "\n"
