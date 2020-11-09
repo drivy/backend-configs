b5561669d989bbdbd83179122176782d60840352
godric <lahumez@gmail.com>
2020-11-09 12:32:12 +0100

    Remove HTTP/Async reporting capabilities

--- getaround_utils/lib/getaround_utils/utils.rb
+++ getaround_utils/lib/getaround_utils/utils.rb
@@ -1,4 +1,1 @@
-require 'getaround_utils/utils/async_queue'
 require 'getaround_utils/utils/deep_key_value'
-require 'getaround_utils/utils/http_reporter'
-require 'getaround_utils/utils/captur_reporter'
