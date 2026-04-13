e63691bfa9fe8f5a67dd839256334184f4a0be1a
Guillaume Chateaux <guillaume.chateaux@getaround.com>
2026-04-13 22:04:14 +0200

    Introduce GetaroundUtils::Utils::HealthCheckFile

---
+++ getaround_utils/lib/getaround_utils/utils/health_check_file.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'pathname'
+
+module GetaroundUtils; end
+
+module GetaroundUtils::Utils; end
+
+class GetaroundUtils::Utils::HealthCheckFile
+  FILE_NAME = 'health_check_file'
+
+  attr_reader :path
+
+  def initialize(name)
+    raise ArgumentError, "Expected name as String, got: #{name.inspect}" if !name.is_a?(String) || name.empty?
+
+    @base_path = root_path.join('tmp', name)
+    @path = @base_path.join(FILE_NAME)
+  end
+
+  def create!
+    FileUtils.mkdir_p(@base_path)
+    FileUtils.touch(@path)
+  end
+
+  def drop!
+    FileUtils.rm_f(@path)
+  end
+
+  def exists?
+    File.exist?(@path)
+  end
+
+  private
+
+  def root_path
+    defined?(::Rails) ? Rails.root : Pathname.new('/')
+  end
+end
---
+++ getaround_utils/spec/getaround_utils/utils/health_check_file_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe GetaroundUtils::Utils::HealthCheckFile do
+  subject(:health_file) { described_class.new(name) }
+
+  let(:name) { 'dummy' }
+
+  let(:root_path) { Pathname.new(ENV.fetch('CI_WORKING_DIR', '/')) }
+  let(:expected_base_path) { root_path.join('tmp', name) }
+  let(:expected_file_path) { expected_base_path.join(described_class::FILE_NAME) }
+
+  shared_context 'with clean filesystem' do
+    around do |example|
+      FileUtils.rm_rf(expected_base_path)
+      example.run
+      FileUtils.rm_rf(expected_base_path)
+    end
+  end
+
+  context 'with invalid name' do
+    let(:name) { '' }
+
+    it { expect { subject }.to raise_error(ArgumentError, /Expected name as String/) }
+  end
+
+  describe '#path' do
+    subject { health_file.path }
+
+    it { is_expected.to eq(expected_file_path) }
+
+    context 'with Rails defined' do
+      let(:root_path) { Pathname.new('/dummy') }
+
+      before do
+        stub_const('Rails', double(root: root_path))
+      end
+
+      it { is_expected.to eq(expected_file_path) }
+    end
+  end
+
+  describe '#create!' do
+    subject { health_file.create! }
+
+    include_context 'with clean filesystem'
+
+    it 'creates the file' do
+      expect(File.exist?(expected_file_path)).to be false
+      subject
+      expect(File.exist?(expected_file_path)).to be true
+    end
+  end
+
+  describe '#drop!' do
+    subject { health_file.drop! }
+
+    include_context 'with clean filesystem'
+
+    before { health_file.create! }
+
+    it 'removes the file' do
+      expect(File.exist?(expected_file_path)).to be true
+      subject
+      expect(File.exist?(expected_file_path)).to be false
+    end
+  end
+
+  describe '#exists?' do
+    subject { health_file.exists? }
+
+    include_context 'with clean filesystem'
+
+    context 'when file is not yet created' do
+      it { is_expected.to be false }
+    end
+
+    context 'when file was already created' do
+      before { health_file.create! }
+
+      it { is_expected.to be true }
+    end
+  end
+end
